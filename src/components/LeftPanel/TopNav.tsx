a5dab276b53c75490b64e92fbed8111e93d3c21e
Faylfire <Faylfire@users.noreply.github.com>
2024-08-31 15:57:05 -0500

    Updated TopNav
    - Added 'ContextAlly' Name to be displayed.

--- src/components/LeftPanel/TopNav.tsx
+++ src/components/LeftPanel/TopNav.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import reactLogo from '@/assets/react.svg'
+import React from "react";
+import reactLogo from "@/assets/react.svg";
 //import NavBar from './NavBar';
 //import TopicList from './TopicList';
 
 function TopNav() {
   return (
-    <div className='bg-black p-2'>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-black p-2 flex gap-4 items-center">
+      <a href="https://react.dev" target="_blank">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+      <h1>ContextAlly</h1>
     </div>
   );
 }
